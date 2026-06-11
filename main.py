a7a2deb29b7d4a139f0d90e63a9af77603db67c7
prince nyarko <prinako.dev+github@gmail.com>
2026-06-11 18:44:29 -0300

    feat: Increase default epoch count for training from 100 to 600
    This commit updates the default value for the --epochs argument in main.py, enhancing the training duration for models to improve performance and convergence during training.

--- main.py
+++ main.py
@@ -31,8 +31,7 @@ parser.add_argument('--dff', default=512, type=int)
 parser.add_argument('--num-layers', default=4, type=int)
 parser.add_argument('--num-heads', default=8, type=int)
 parser.add_argument('--batch-size', default=128, type=int)
-parser.add_argument('--epochs', default=100, type=int)
-
+parser.add_argument('--epochs', default=600, type=int) 
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
