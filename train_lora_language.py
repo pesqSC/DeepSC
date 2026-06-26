6fd13a92ec2ab97c827b7b29ec2639389fd79e5c
prince nyarko <prinako.dev+github@gmail.com>
2026-06-26 14:12:20 -0300

    feat: Refactor training loop to integrate transmitter and LoRA model
    This commit updates the `train_lora_epoch` function to accept a transmitter and LoRA model, enhancing the training process for multilingual models. It modifies the data loading and training logic, including the use of tqdm for progress tracking. Additionally, it updates the command-line arguments for better clarity and adjusts the default batch size and number of layers for improved training efficiency.

--- train_lora_language.py
+++ train_lora_language.py
@@ -2,41 +2,61 @@ import argparse
 import json
 import torch
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from dataset_multilingual import EurParallelDataset, collate_parallel
 from models.transceiver import DeepSC
 from models.lora import apply_lora_to_decoder, lora_parameters, save_lora
-from utils import SNR_to_noise, create_masks
+from utils import SNR_to_noise, create_masks, setup_seed
+from student import Student
+from models.tx_model import Transmitter
 
 
-def train_lora_epoch(model, loader, optimizer, device, pad_idx, channel="AWGN", snr=12):
-    model.train()
+def train_lora_epoch(epoch, transmitter, LoraModel, loader, optimizer, device, pad_idx, channel="AWGN", snr=12):
+    LoraModel.train()
     total_loss = 0
 
     noise_std = SNR_to_noise(snr)
+    
+    pbar = tqdm(loader)
 
-    for src, trg in loader:
+    for src, trg in pbar:
         src = src.to(device)
         trg = trg.to(device)
 
-        trg_input = trg[:, :-1]
+        trg_inp = trg[:, :-1]
         trg_real = trg[:, 1:]
 
-        src_mask, look_ahead_mask = create_masks(src, trg_input, pad_idx)
+        src_mask, look_ahead_mask = create_masks(src, trg_inp, pad_idx)
 
         optimizer.zero_grad()
 
-        logits = model(
-            src,
-            trg_input,
-            src_mask,
-            look_ahead_mask,
-            noise_std,
-            channel
+        with torch.no_grad():
+            tx_en_out, tx_ch_en_out, Tx_sig, z_noisy = transmitter(
+                src, 
+                src_mask, 
+                channel, 
+                noise_std
+            )
+
+        l1_logits, l1_ch_dec_out, l1_dec_out = LoraModel(
+            z_noisy, 
+            trg_inp, 
+            look_ahead_mask, 
+            src_mask
         )
 
+        # logits = transmitter(
+        #     src,
+        #     trg_input,
+        #     src_mask,
+        #     look_ahead_mask,
+        #     noise_std,
+        #     channel
+        # )
+
         loss = torch.nn.functional.cross_entropy(
-            logits.reshape(-1, logits.size(-1)),
+            l1_logits.reshape(-1, l1_logits.size(-1)),
             trg_real.reshape(-1),
             ignore_index=pad_idx
         )
@@ -45,39 +65,52 @@ def train_lora_epoch(model, loader, optimizer, device, pad_idx, channel="AWGN", 
         optimizer.step()
 
         total_loss += loss.item()
+        pbar.set_description(f"Epoch {epoch + 1} Train; Loss: {loss.item():.5f}")
 
-    return total_loss / len(loader)
+    return LoraModel, (total_loss / len(loader))
 
 
 def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--vocab-file", default="data/train/europarl/vocab_multilingual.json")
-    parser.add_argument("--train-data", required=True)
-    parser.add_argument("--student-checkpoint", required=True)
-    parser.add_argument("--save-lora", required=True)
+    # parser.add_argument("--train-data", required=True)
+    parser.add_argument("--student-checkpoint", default="./checkpoints/deepsc-Rayleigh/multi_vocab_kd")
+    parser.add_argument("--transmitter-checkpoint", type=str, default="./checkpoints/deepsc-Rayleigh/muiltilingual")
+    parser.add_argument("--save-lora", default="./checkpoints/deepsc-Rayleigh/lora")
 
-    parser.add_argument("--channel", default="AWGN")
+    parser.add_argument("--channel", default="Rayleigh", type=str, choices=["AWGN", "Rayleigh", "Rician"])
     parser.add_argument("--snr", default=12, type=float)
     parser.add_argument("--epochs", default=10, type=int)
-    parser.add_argument("--batch-size", default=64, type=int)
+    parser.add_argument("--batch-size", default=128, type=int)
     parser.add_argument("--lr", default=1e-4, type=float)
 
-    parser.add_argument("--num-layers", default=4, type=int)
+    parser.add_argument("--num-layers", default=6, type=int)
+    parser.add_argument("--num-heads", default=8, type=int)
     parser.add_argument("--d-model", default=128, type=int)
     parser.add_argument("--dff", default=512, type=int)
-    parser.add_argument("--num-heads", default=8, type=int)
+
+    parser.add_argument('--en', default='en_en', type=str)
+    parser.add_argument('--en-pt', default='en_pt', type=str)
+    parser.add_argument('--en-es', default='en_es', type=str)
+    parser.add_argument('--en-fr', default='en_fr', type=str)
+
 
     args = parser.parse_args()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    setup_seed(42)
 
     vocab = json.load(open(args.vocab_file, "r", encoding="utf-8"))
     token_to_idx = vocab["token_to_idx"]
     vocab_size = len(token_to_idx)
     pad_idx = token_to_idx["<PAD>"]
 
-    dataset = EurParallelDataset(args.train_data)
+    train_lag = args.en_pt
+
+    dataset = EurParallelDataset(train_lag, 'train')
+
     loader = DataLoader(
         dataset,
         batch_size=args.batch_size,
@@ -85,8 +118,22 @@ def main():
         collate_fn=lambda b: collate_parallel(b, pad_idx)
     )
 
-    model = DeepSC(
+    deepsc = DeepSC(
         args.num_layers,
+        vocab_size,
+        vocab_size,
+        vocab_size,
+        vocab_size,
+        args.d_model,
+        args.num_heads,
+        args.dff,
+        0.1
+    )
+    
+    transmitter = Transmitter(deepsc.encoder, deepsc.channel_encoder).to(device)
+
+    student =  Student(
+        2,
         vocab_size,
         vocab_size,
         vocab_size,
@@ -97,15 +144,45 @@ def main():
         0.1
     ).to(device)
 
-    model.load_state_dict(torch.load(args.student_checkpoint, map_location=device))
+    enc_model_path = os.path.join(args.transmitter_checkpoint, 'encoder_200.pth')
+    # dec_model_path = 'decoder_20.pth'
+    # student_path = 'checkpoints/tr_kd/student_tr_best.pth'
+    student_1_path = os.path.join(args.student_checkpoint, 'encoder_200.pth')
+    student_2_path = 'student_2_mult_best.pth'
 
-    model = apply_lora_to_decoder(model, r=8, alpha=16, dropout=0.05)
+    # enc_checkpoint = torch.load(os.path.join(my_vars.checkpoint_path, enc_model_path), map_location=device)
+    # dec_checkpoint = torch.load(os.path.join(my_vars.checkpoint_path, dec_model_path), map_location=device)
 
-    optimizer = torch.optim.Adam(lora_parameters(model), lr=args.lr)
+    enc_checkpoint = torch.load(enc_model_path, map_location=device)
+    # dec_checkpoint = torch.load("decoder_200.pth", map_location=device)
+    
+    # Ecoder
+    transmitter.encoder.load_state_dict(enc_checkpoint['encoder'])
+    transmitter.channel_encoder.load_state_dict(enc_checkpoint['channel_encoder'])
 
-    for epoch in range(args.epochs):
-        loss = train_lora_epoch(
-            model,
+    student_1_checkpoint = torch.load(student_1_path, map_location=device)
+
+    # Student
+    student.channel_decoder.load_state_dict(student_1_checkpoint['channel_decoder'])
+    student.decoder.load_state_dict(student_1_checkpoint['decoder'])
+    student.dense.load_state_dict(student_1_checkpoint['dense'])
+
+    # Decoder
+    # deepsc.channel_decoder.load_state_dict(dec_checkpoint['channel_decoder'])
+    # deepsc.decoder.load_state_dict(dec_checkpoint['decoder'])
+    # deepsc.dense.load_state_dict(dec_checkpoint['dense'])
+
+
+    LoRA = apply_lora_to_decoder(student, r=8, alpha=16, dropout=0.05).to(device)
+
+    optimizer = torch.optim.Adam(lora_parameters(student), lr=args.lr)
+    
+    pbar = tqdm(range(args.epochs))
+    for epoch in pbar:
+        model, loss = train_lora_epoch(
+            epoch,
+            transmitter,
+            LoRA,
             loader,
             optimizer,
             device,
@@ -114,10 +191,10 @@ def main():
             args.snr
         )
 
-        print(f"Epoch {epoch + 1}/{args.epochs} | LoRA loss: {loss:.4f}")
+        pbar.set_description(f"Epoch {epoch + 1}/{args.epochs} | LoRA loss: {loss:.4f}")
 
-    save_lora(model, args.save_lora)
-    print(f"Saved LoRA adapter to {args.save_lora}")
+        save_lora(epoch, model, args.save_lora, train_lag)
+        print(f"Saved LoRA adapter to {args.save_lora}")
 
 
 if __name__ == "__main__":
